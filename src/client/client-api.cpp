ce2d2dfb158b4368c5b3232e920b4832d883e4d3
Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
2014-05-08 16:27:51 +0200

    Implement cynara-bootstrap version of libcynara-client
    This is temporary version - it's implementation is based on
    security-server and libprivilege-control instead of cynara service.
    
    [Bug/Feature]   N/A
    [Cause]         First working version of cynara's client.
    [Solution]      Implementation temporary based on security-server.
    [Verification]  Should be build along with all other patches
                    marked with topic:cynara-bootstrap.
    		Build needs: http://review.tizen.org/gerrit/#/c/20519/
    
    Change-Id: I8468b6756c9844840f3b3bea37f498b08b7f7188
    Signed-off-by: Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>

---
+++ src/client/client-api.cpp
@@ -0,0 +1,72 @@
+/*
+ *  Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        client-api.cpp
+ * @author      Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
+ * @version     1.0
+ * @brief       Implementation of external libcynara-client API
+ */
+
+#include <new>
+#include <common.h>
+#include <cynara-client.h>
+#include <cynara-client-interface.h>
+#include <bootstrap/cynara-client-bootstrap.h>
+
+struct cynara {
+    CynaraClientInterface* impl;
+
+    cynara(CynaraClientInterface *_impl) : impl(_impl) {
+    }
+    ~cynara() {
+        delete impl;
+    }
+};
+
+CYNARA_API
+int cynara_initialize(cynara **pp_cynara, const cynara_configuration *p_conf UNUSED)
+{
+    if (!pp_cynara)
+        return cynara_api_result::CYNARA_API_INVALID_PARAM;
+
+    try {
+        *pp_cynara = new cynara(new CynaraClientBootstrap);
+    } catch (std::bad_alloc& ex) {
+        return cynara_api_result::CYNARA_API_OUT_OF_MEMORY;
+    }
+
+    return cynara_api_result::CYNARA_API_SUCCESS;
+}
+
+CYNARA_API
+int cynara_finish(cynara *p_cynara)
+{
+    delete p_cynara;
+
+    return cynara_api_result::CYNARA_API_SUCCESS;
+}
+
+CYNARA_API
+int cynara_check(cynara *p_cynara, const char *client, const char *client_session, const char *user,
+    const char *privilege)
+{
+    if(!p_cynara || !p_cynara->impl)
+        return cynara_api_result::CYNARA_API_INVALID_PARAM;
+    if(!client || !client_session || !user || !privilege)
+        return cynara_api_result::CYNARA_API_INVALID_PARAM;
+
+    return p_cynara->impl->check(client, client_session, user, privilege);
+}
