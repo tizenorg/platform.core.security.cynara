f404f7d5fd96f2aa2b45e9726977f2e11ea285a6
Pawel Wieczorek <p.wieczorek2@samsung.com>
2014-08-27 09:18:02 +0200

    Implement mechanism assuring integrity of database
    There is also added mechanism for cleaning up Cynara's database
    directory upon loading policies to memory.
    
    There are added tests checking whether mechanism behaves as intended.
    
    Change-Id: I926d1aebf394c092e00731b73717e0e1c55bad0c

--- test/storage/inmemorystoragebackend/inmemorystoragebackend.cpp
+++ test/storage/inmemorystoragebackend/inmemorystoragebackend.cpp
@@ -192,9 +192,12 @@ TEST_F(InMemeoryStorageBackendFixture, deletePolicyFromNonexistentBucket) {
 // Database dir is empty
 TEST_F(InMemeoryStorageBackendFixture, load_no_db) {
     using ::testing::ReturnRef;
+    using ::testing::Return;
     auto testDbPath = std::string(CYNARA_TESTS_DIR) + "/db1/";
     FakeInMemoryStorageBackend backend(testDbPath);
+    EXPECT_CALL(backend, backupGuardExists()).WillOnce(Return(false));
     EXPECT_CALL(backend, buckets()).WillRepeatedly(ReturnRef(m_buckets));
+    EXPECT_CALL(backend, deleteNonIndexedFiles()).WillOnce(Return());
     backend.load();
     ASSERT_DB_VIRGIN(m_buckets);
 }
@@ -202,9 +205,12 @@ TEST_F(InMemeoryStorageBackendFixture, load_no_db) {
 // Database dir contains index with default bucket, but no file for this bucket
 TEST_F(InMemeoryStorageBackendFixture, load_no_default) {
     using ::testing::ReturnRef;
+    using ::testing::Return;
     auto testDbPath = std::string(CYNARA_TESTS_DIR) + "/db2/";
     FakeInMemoryStorageBackend backend(testDbPath);
+    EXPECT_CALL(backend, backupGuardExists()).WillOnce(Return(false));
     EXPECT_CALL(backend, buckets()).WillRepeatedly(ReturnRef(m_buckets));
+    EXPECT_CALL(backend, deleteNonIndexedFiles()).WillOnce(Return());
     backend.load();
     ASSERT_DB_VIRGIN(m_buckets);
 }
@@ -212,9 +218,12 @@ TEST_F(InMemeoryStorageBackendFixture, load_no_default) {
 // Database contains index with default bucket and an empty bucket file
 TEST_F(InMemeoryStorageBackendFixture, load_default_only) {
     using ::testing::ReturnRef;
+    using ::testing::Return;
     auto testDbPath = std::string(CYNARA_TESTS_DIR) + "/db3/";
     FakeInMemoryStorageBackend backend(testDbPath);
+    EXPECT_CALL(backend, backupGuardExists()).WillOnce(Return(false));
     EXPECT_CALL(backend, buckets()).WillRepeatedly(ReturnRef(m_buckets));
+    EXPECT_CALL(backend, deleteNonIndexedFiles()).WillOnce(Return());
     backend.load();
     ASSERT_DB_VIRGIN(m_buckets);
 }
@@ -223,12 +232,15 @@ TEST_F(InMemeoryStorageBackendFixture, load_default_only) {
 // There are files for both buckets present
 TEST_F(InMemeoryStorageBackendFixture, load_2_buckets) {
     using ::testing::ReturnRef;
+    using ::testing::Return;
     using ::testing::IsEmpty;
 
     auto testDbPath = std::string(CYNARA_TESTS_DIR) + "/db4/";
 
     FakeInMemoryStorageBackend backend(testDbPath);
+    EXPECT_CALL(backend, backupGuardExists()).WillOnce(Return(false));
     EXPECT_CALL(backend, buckets()).WillRepeatedly(ReturnRef(m_buckets));
+    EXPECT_CALL(backend, deleteNonIndexedFiles()).WillOnce(Return());
     backend.load();
 
     std::vector<std::string> bucketIds = { "", "additional" };
@@ -246,9 +258,88 @@ TEST_F(InMemeoryStorageBackendFixture, load_2_buckets) {
 // Database contains index with 2 buckets; 1st bucket is valid, but second is corrupted
 TEST_F(InMemeoryStorageBackendFixture, second_bucket_corrupted) {
     using ::testing::ReturnRef;
+    using ::testing::Return;
     auto testDbPath = std::string(CYNARA_TESTS_DIR) + "/db5/";
     FakeInMemoryStorageBackend backend(testDbPath);
+    EXPECT_CALL(backend, backupGuardExists()).WillOnce(Return(false));
     EXPECT_CALL(backend, buckets()).WillRepeatedly(ReturnRef(m_buckets));
+    EXPECT_CALL(backend, deleteNonIndexedFiles()).WillOnce(Return());
     backend.load();
     ASSERT_DB_VIRGIN(m_buckets);
+}
+/**
+ * @brief   Database was corrupted, restore from backup (which is present)
+ * @test     Scenario:
+ * - There still is guard file in earlier prepared Cynara's policy database directory (db5)
+ * - Execution of load() should use backup files (present, with different contents than primaries)
+ * - We make sure backupGuardExists() and revalidatePrimaryDatabase() are being called
+ *   (it's crucial they are, implemented integrity mechanism depend on it)
+ * - Loaded database is checked - backup files were empty, so should recently loaded policies
+ */
+TEST_F(InMemeoryStorageBackendFixture, load_from_backup) {
+    using ::testing::ReturnRef;
+    using ::testing::Return;
+    using ::testing::IsEmpty;
+
+    auto testDbPath = std::string(CYNARA_TESTS_DIR) + "/db6/";
+    FakeInMemoryStorageBackend backend(testDbPath);
+
+    EXPECT_CALL(backend, backupGuardExists()).WillOnce(Return(true));
+    EXPECT_CALL(backend, buckets()).WillRepeatedly(ReturnRef(m_buckets));
+    EXPECT_CALL(backend, revalidatePrimaryDatabase()).WillOnce(Return());
+    EXPECT_CALL(backend, deleteNonIndexedFiles()).WillOnce(Return());
+    backend.load();
+
+    std::vector<std::string> bucketIds = { "", "additional" };
+    for(const auto &bucketId : bucketIds) {
+        SCOPED_TRACE(bucketId);
+        const auto bucketIter = m_buckets.find(bucketId);
+        ASSERT_NE(m_buckets.end(), bucketIter);
+
+        const auto &bucketPolicies = bucketIter->second;
+        ASSERT_THAT(bucketPolicies, IsEmpty());
+    }
+}
+/**
+ * @brief   Database successfully dumped to storage
+ * @test    Scenario:
+ * - Sample database is loaded from earlier prepared Cynara's policy database directory (db6)
+ * - Modification in one of the buckets is made (involves writing changes to storage)
+ * - We make sure createBackupGuard() and revalidatePrimaryDatabase() are being called
+ * - Due to execution of mocked database revalidation procedure backup files should not be removed
+ *   from storage
+ * - Check whether backup files are still present - if so, changes were actually saved using them
+ *   as temporary data placeholder
+ *
+ * This test will fail if run as non-root user due to writing in CYNARA_TESTS_DIR
+ * Possible solution: copy CYNARA_TESTS_DIR/db6 to /tmp/db6 and initialize backend with /tmp path
+ */
+TEST_F(InMemeoryStorageBackendFixture, save_assuring_db_integrity) {
+    using ::testing::ReturnRef;
+    using ::testing::Return;
+    using PredefinedPolicyType::ALLOW;
+
+    auto testDbPath = std::string(CYNARA_TESTS_DIR) + "/db7/";
+    FakeInMemoryStorageBackend backend(testDbPath);
+
+    EXPECT_CALL(backend, backupGuardExists()).WillOnce(Return(false));
+    EXPECT_CALL(backend, buckets()).WillRepeatedly(ReturnRef(m_buckets));
+    EXPECT_CALL(backend, deleteNonIndexedFiles()).WillOnce(Return());
+    backend.load();
+
+    auto policyToAdd = Policy::simpleWithKey(Helpers::generatePolicyKey(), ALLOW);
+    backend.insertPolicy("additional", policyToAdd);
+
+    EXPECT_CALL(backend, createBackupGuard()).WillOnce(Return());
+    EXPECT_CALL(backend, revalidatePrimaryDatabase()).WillOnce(Return());
+    backend.save();
+
+    std::vector<std::string> fileNames = { "buckets~", "_~", "_additional~" };
+    for (const auto &bucketFileName : fileNames) {
+        SCOPED_TRACE(bucketFileName);
+        ASSERT_TRUE(Helpers::fileExists(testDbPath + bucketFileName));
+    }
+// maybe add manual database revalidation after testing is finished?
+// (had to mock revalidation procedure for unit-test result reproduction - without it
+// 'load_from_backup' test could be run only once per cynara-tests package installation)
 }
