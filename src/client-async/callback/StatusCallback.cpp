c01c2a692279840dd1dc438357963c9f90d27ab5
Marcin Niesluchowski <m.niesluchow@samsung.com>
2014-09-09 11:24:00 +0200

    Add connection check
    Every api function except cynara_async_initialize and cynara_async_finish
    should check connection with cynara before executing further code.
    If disconnected it should try reconnecting and return following codes
    on failure:
    - CYNARA_API_CACHE_MISS            for cynara_async_cache_check
    - CYNARA_API_SERVICE_NOT_AVAILABLE for other functions.
    
    Change-Id: I2920f4dbd0bde3e7be74096db33f4ff465bc5028

---
+++ src/client-async/callback/StatusCallback.cpp
@@ -0,0 +1,54 @@
+/*
+ *  Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        StatusCallback.h
+ * @author      Marcin Niesluchowski <m.niesluchow@samsung.com>
+ * @version     1.0
+ * @brief       This file contains definition of StatusCallback class
+ */
+
+#include "StatusCallback.h"
+
+namespace Cynara {
+
+StatusCallback::StatusCallback(cynara_status_callback callback, void *userData)
+    : m_callback(callback),  m_userData(userData), m_sockFd(-1)
+{
+}
+
+void StatusCallback::onStatusChange(int newFd, cynara_async_status status)
+{
+    if (!m_callback)
+        return;
+    m_callback(m_sockFd,
+               newFd,
+               status,
+               m_userData);
+    m_sockFd = newFd;
+}
+
+void StatusCallback::onDisconnected()
+{
+    if (!m_callback)
+        return;
+    m_callback(m_sockFd,
+               -1,
+               static_cast<cynara_async_status>(0),
+               m_userData);
+    m_sockFd = -1;
+}
+
+} // namespace Cynara
