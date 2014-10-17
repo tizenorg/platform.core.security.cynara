b4a4d8005a9bc2c6851ede88021512fe1d749c47
Zofia Abramowska <z.abramowska@samsung.com>
2014-10-17 19:22:33 +0200

    Add test Service and Client plugins
    Change-Id: I55bd24d981042fdd449f59216a5262547f2b8144

---
+++ src/plugin/SupportedTypes.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <types/PolicyType.h>
+
+namespace SupportedTypes {
+
+namespace Service {
+//service
+const Cynara::PolicyType ASK_USER = 13;
+}
+
+namespace Client {
+//client
+const Cynara::PolicyType ALLOW_ONCE = 11;
+const Cynara::PolicyType ALLOW_PER_SESSION = 12;
+const Cynara::PolicyType ALLOW_PER_LIFE = 13;
+}
+}
