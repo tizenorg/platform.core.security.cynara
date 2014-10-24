4fc9a9196e47b73f2306210ec4595a1f733f4625
Adam Malinowski <a.malinowsk2@partner.samsung.com>
2014-10-24 16:05:58 +0200

    Add agent manager
    This commit introduces AgentManager class which will help plugins
    in communication with agents. Also registering and unregistering agents
    will be handled by this class.
    
    Change-Id: Id3f7e5785223c3b0316f97bc8107805572a0fd10

--- src/service/logic/Logic.cpp
+++ src/service/logic/Logic.cpp
@@ -49,6 +49,7 @@
 #include <storage/Storage.h>
 
 #include <main/Cynara.h>
+#include <agent/AgentManager.h>
 #include <sockets/SocketManager.h>
 
 #include "Logic.h"
@@ -80,9 +81,9 @@ void Logic::execute(RequestContextPtr context, AdminCheckRequestPtr request) {
 }
 
 void Logic::execute(RequestContextPtr context, AgentRegisterRequestPtr request) {
-    // MOCKUP
+    auto result = m_agentManager->registerAgent(request->agentType(), context->responseQueue());
     context->returnResponse(context, std::make_shared<AgentRegisterResponse>(
-                            AgentRegisterResponse::DONE, request->sequenceNumber()));
+                            result, request->sequenceNumber()));
 }
 
 void Logic::execute(RequestContextPtr context, CancelRequestPtr request) {
