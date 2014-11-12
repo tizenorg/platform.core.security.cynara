a0f01ca45bbd22b718f7b518399f8fc0988f0667
Zofia Abramowska <z.abramowska@samsung.com>
2014-11-12 16:30:35 +0100

    Remove unnecessary Protocol shared pointers
    ProtocolFramePtr and ProtocolFrameHeaderPtr was used unnecessary
    (no shared ownership required).
    
    Change-Id: I71d4bf797450a46bc35e2321ff8d01a6508bcf88

--- src/common/protocol/ProtocolFrame.h
+++ src/common/protocol/ProtocolFrame.h
@@ -39,10 +39,10 @@ class ProtocolFrameSerializer;
 class ProtocolFrame: public IStream {
 
 public:
-    ProtocolFrame(ProtocolFrameHeaderPtr frameHeader, BinaryQueuePtr headerContent);
+    ProtocolFrame(ProtocolFrameHeader frameHeader, BinaryQueuePtr headerContent);
     virtual ~ProtocolFrame() {};
 
-    ProtocolFrameHeaderPtr frameHeader(void) {
+    ProtocolFrameHeader &frameHeader(void) {
         return m_frameHeader;
     }
 
@@ -50,7 +50,7 @@ public:
     virtual void write(size_t num, const void *bytes);
 
 private:
-    ProtocolFrameHeaderPtr m_frameHeader;
+    ProtocolFrameHeader m_frameHeader;
     BinaryQueuePtr m_frameBodyContent;
 
     BinaryQueue &bodyContent(void) {
