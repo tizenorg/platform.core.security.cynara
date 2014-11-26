faaf67ef4f5875d82b4ce9458683c06b95f922a2
Aleksander Zdyb <a.zdyb@samsung.com>
2014-11-26 09:23:06 +0100

    Add DatabaseLock
    Change-Id: Ia5fa4ef51e1a413beb81efd56f807e1434145691

---
+++ src/common/lock/FileLock.cpp
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/**
+ * @file        src/common/lock/FileLock.cpp
+ * @author      Aleksander Zdyb <a.zdyb@samsung.com>
+ * @version     1.0
+ */
+
+
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <exceptions/CannotCreateFileException.h>
+#include <exceptions/FileLockAcquiringException.h>
+#include <log/log.h>
+
+#include "FileLock.h"
+
+namespace Cynara {
+
+FileLock::FileLock(const std::string &lockFilename)
+    : m_lockFilename(lockFilename)
+{
+    m_fd = ::open(m_lockFilename.c_str(), O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR);
+    if (m_fd < 0)
+        throw CannotCreateFileException(m_lockFilename);
+
+    LOGD("Database lock file opened");
+}
+
+FileLock::~FileLock() {
+    ::close(m_fd);
+}
+
+bool FileLock::tryLock(void) {
+    int lock = TEMP_FAILURE_RETRY(::flock(m_fd, LOCK_EX | LOCK_NB));
+
+    if (lock == 0) {
+        LOGI("Database lock acquired");
+        return true;
+    } else if (errno == EWOULDBLOCK) {
+        LOGI("Database lock NOT acquired");
+        return false;
+    }
+
+    throw FileLockAcquiringException(errno);
+}
+
+void FileLock::lock(void) {
+    int lock = TEMP_FAILURE_RETRY(::flock(m_fd, LOCK_EX));
+
+    if (lock == -1)
+        throw FileLockAcquiringException(errno);
+
+    LOGI("Database lock acquired");
+}
+
+} /* namespace Cynara */
