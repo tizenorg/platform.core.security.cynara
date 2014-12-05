d0b77b2cc91759b9d34fb46b118e4232347c6b23
Aleksander Zdyb <a.zdyb@samsung.com>
2014-12-05 07:57:18 +0100

    Introduce Cyad
    A commandline tool for managing Cynara's database.
    
    For now, only --help option is recognized, but no
    actual functionality is implemented.
    
    Change-Id: I70e50291ba226875620c8fd08fb5ec9d59f0a7ec

---
+++ src/cyad/CommandlineParser/ParsingResult.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/*
+ * @file        src/cyad/CommandlineParser/ParsingResult.h
+ * @author      Aleksander Zdyb <a.zdyb@samsung.com>
+ * @version     1.0
+ * @brief       ParsingResult class
+ */
+
+#ifndef SRC_CYAD_COMMANDLINEPARSER_PARSINGRESULT_H_
+#define SRC_CYAD_COMMANDLINEPARSER_PARSINGRESULT_H_
+
+#include <string>
+
+#include <types/PolicyBucketId.h>
+#include <types/PolicyResult.h>
+#include <types/PolicyType.h>
+
+namespace Cynara {
+
+class Cyad;
+
+class ParsingResult {
+public:
+    ParsingResult() = default;
+    virtual ~ParsingResult() = default;
+
+    virtual void run(Cyad &cyad);
+
+    virtual bool isError(void) const {
+        return false;
+    }
+};
+
+class ErrorParsingResult : public ParsingResult {
+public:
+    ErrorParsingResult(const std::string &message) : m_message(message) {};
+    virtual ~ErrorParsingResult() = default;
+
+    virtual void run(Cyad &cyad);
+
+    virtual bool isError(void) const {
+        return true;
+    }
+
+    virtual const std::string &message(void) const {
+        return m_message;
+    }
+
+private:
+    std::string m_message;
+};
+
+class HelpParsingResult : public ParsingResult {
+public:
+    using ParsingResult::ParsingResult;
+
+    virtual void run(Cyad &cyad);
+};
+
+} /* namespace Cynara */
+
+#endif /* SRC_CYAD_COMMANDLINEPARSER_PARSINGRESULT_H_ */
---
+++ test/cyad/CyadCommandlineTest.h
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/*
+ * @file        CyadCommandlineTest.h
+ * @author      Aleksander Zdyb <a.zdyb@samsung.com>
+ * @version     1.0
+ * @brief       Fixture for CyadCommandlineParser tests
+ */
+#ifndef TEST_CYAD_CYADCOMMANDLINETEST_H_
+#define TEST_CYAD_CYADCOMMANDLINETEST_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <cyad/CommandlineParser/CyadCommandlineParser.h>
+#include <cyad/CommandlineParser/ParsingResult.h>
+
+class CyadCommandlineTest : public ::testing::Test {
+public:
+    typedef std::vector<std::string> Args;
+
+    void prepare_argv(const Args &args) {
+        destory_argv();
+
+        m_argc = args.size();
+        m_argv = (char **)malloc(m_argc * sizeof(char *));
+        for (auto i = 0; i < m_argc; ++i) {
+            m_argv[i] = strdup(args.at(i).c_str());
+        }
+    }
+
+    int argc(void) const {
+        return m_argc;
+    }
+
+    char * const *argv(void) const {
+        return m_argv;
+    }
+
+protected:
+    virtual void TearDown(void) {
+        destory_argv();
+    }
+
+    void destory_argv(void) {
+        for (auto i = 0; i < m_argc; ++i) {
+            free(m_argv[i]);
+        }
+        free(m_argv);
+
+        m_argc = 0;
+        m_argv = nullptr;
+    }
+
+private:
+    int m_argc = 0;
+    char **m_argv = nullptr;
+};
+
+#endif /* TEST_CYAD_CYADCOMMANDLINETEST_H_ */
