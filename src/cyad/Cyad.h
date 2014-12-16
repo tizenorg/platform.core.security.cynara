b95245e9048920f7483a0a80fffd2d66a3bd4ab9
Aleksander Zdyb <a.zdyb@samsung.com>
2014-12-16 13:29:02 +0100

    Assemble all Cyad's components
    Change-Id: I1a711cad7d5aba0508ef41f308cddcc7a0704b28

---
+++ src/cyad/Cyad.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/**
+ * @file        src/cyad/Cyad.h
+ * @author      Aleksander Zdyb <a.zdybsamsung.com>
+ * @version     1.0
+ * @brief       A commandline tool to manage Cynara's database
+ */
+
+#ifndef SRC_CYAD_CYAD_H_
+#define SRC_CYAD_CYAD_H_
+
+#include <memory>
+
+#include <cyad/AdminApiWrapper.h>
+#include <cyad/CommandlineParser/CyadCommandlineParser.h>
+#include <cyad/CommandsDispatcher.h>
+#include <cyad/DispatcherIO.h>
+
+namespace Cynara {
+
+class Cyad {
+public:
+    Cyad(int argc, char **argv);
+    virtual ~Cyad();
+
+    CyadExitCode run(void);
+    bool cynaraOperational(void) const;
+
+private:
+    AdminApiWrapper m_adminApiWrapper;
+    DispatcherIO m_io;
+    std::unique_ptr<CommandsDispatcher> m_dispatcher;
+    CyadCommandlineParser m_parser;
+    int m_cynaraInitError;
+};
+
+} /* namespace Cynara */
+
+#endif /* SRC_CYAD_CYAD_H_ */
