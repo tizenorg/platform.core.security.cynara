83ae96023ff10f1b797e8dc0a8e6aa5b3eb749a9
Pawel Wieczorek <p.wieczorek2@samsung.com>
2015-01-21 14:12:42 +0100

    Add version information to the main executable
    In order to comply with GNU Coding Standards for command-line
    interfaces, Cynara has to report its current version.
    
    Cynara now responds to two command-line options:
    
    * -V, --version    prints installed version of Cynara,
    * -h, --help       prints help message.
    
    Change-Id: I386a09d00f1542cbff8db6a4b9eb2ac9a7fab9fb

---
+++ src/service/main/CmdlineParser.cpp
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/**
+ * @file        src/service/main/CmdlineParser.cpp
+ * @author      Pawel Wieczorek <p.wieczorek2@samsung.com>
+ * @version     1.0
+ * @brief       Helper namespace for Cynara's command-line options parsing
+ */
+
+#include <getopt.h>
+#include <iostream>
+#include <sstream>
+
+#include "CmdlineParser.h"
+
+namespace Cynara {
+
+namespace CmdlineParser {
+
+bool parseCmdlineOptions(int argc, char * const *argv) {
+    std::stringstream shortOpts;
+    shortOpts << ":" << CmdlineOpt::Help << CmdlineOpt::Version;
+
+    const struct option longOpts[] = {
+        { "help",       no_argument,    NULL, CmdlineOpt::Help },
+        { "version",    no_argument,    NULL, CmdlineOpt::Version },
+        { NULL, 0, NULL, 0 }
+    };
+
+
+    optind = 0; // On entry to `getopt', zero means this is the first call; initialize.
+    int opt;
+    while ((opt = getopt_long(argc, argv, shortOpts.str().c_str(), longOpts, nullptr)) != -1) {
+        switch (opt) {
+            case CmdlineOpt::Help:
+                printHelp(std::string(argv[0]));
+                return true;
+            case CmdlineOpt::Version:
+                printVersion();
+                return true;
+            case '?': // Unknown option
+                return false;
+            default:
+                return false;
+        }
+    }
+
+    return false;
+}
+
+void printHelp(const std::string &execName) {
+    std::cout << "Usage: " << execName << " [OPTIONS]" << std::endl << std::endl;
+    std::cout << "  -V, --version                  print version of " << execName << std::endl;
+    std::cout << "  -h, --help                     print this help message and exit" << std::endl;
+}
+
+void printVersion(void) {
+    std::cout << std::string(CYNARA_VERSION) << std::endl;
+}
+
+} /* namespace CmdlineOpts */
+
+} /* namespace Cynara */
