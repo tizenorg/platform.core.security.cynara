8bde9e26e49983b69d0977e650fd32aefac1b003
Pawel Wieczorek <p.wieczorek2@samsung.com>
2015-03-03 12:36:13 +0100

    Handle database corruption
    Loading database from storage is now moved to logic layer.
    InMemoryStorageBackend throws DatabaseCorruptedException to trigger
    corrupted state toggle when database corruption is detected.
    
    Tests which involved database to enter corrupted state were updated.
    
    Change-Id: I0df8c38322b4478abb8c6d18c0805f97cfa45161

--- src/storage/InMemoryStorageBackend.cpp
+++ src/storage/InMemoryStorageBackend.cpp
@@ -35,6 +35,7 @@
 #include <log/log.h>
 #include <config/PathConfig.h>
 #include <exceptions/BucketNotExistsException.h>
+#include <exceptions/DatabaseCorruptedException.h>
 #include <exceptions/DatabaseException.h>
 #include <exceptions/FileNotFoundException.h>
 #include <exceptions/UnexpectedErrorException.h>
@@ -91,7 +92,7 @@ void InMemoryStorageBackend::load(void) {
     } catch (const DatabaseException &) {
         LOGC("Reading cynara database failed.");
         buckets().clear();
-        // TODO: Implement emergency mode toggle
+        throw DatabaseCorruptedException();
     }
     m_checksum.clear();
 
