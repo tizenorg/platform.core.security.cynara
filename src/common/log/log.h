254244f58f70166b1fdd9d26a9bc6fd205d9d195
Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
2015-04-17 21:00:22 +0200

    Secure logs format string
    This patch makes compilers happy. Some of them tries to shout
    if -werror=format-security flag was enabled about unsecure format
    string that is created using stringstream.
    
    This patch defines own format string "%s" if there are no additional
    parameters except format string passed to LOG... macros.
    
    Dark sides of this patch are:
    * now LOG macros are limited to 64 parameters (only for c-style logging,
        e.g. LOGD("%d %s", 5, "cats were hanged on the wall"); )
    * usage of %m is not supported if there are additional arguments.
    
    Change-Id: I2d4ab5a07d170c85f162a5f59cb63ecbfb56fec6

--- src/common/log/log.h
+++ src/common/log/log.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ * Copyright (c) 2014-2015 Samsung Electronics Co., Ltd All Rights Reserved
  *
  *  Contact: Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
  *
@@ -18,12 +18,38 @@
 /**
  * @file        src/common/log/log.h
  * @author      Adam Malinowski <a.malinowsk2@partner.samsung.com>
+ * @author      Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
  * @version     1.0
  * @brief       This file defines logging utilities.
  */
 
 #ifndef CYNARA_COMMON_LOG_H
 #define CYNARA_COMMON_LOG_H
+
+#define __LOG_NARG(...) \
+         __LOG_NARG_(__VA_ARGS__, __LOG_RSEQ_N())
+
+#define __LOG_NARG_(...) \
+         __LOG_ARG_N(__VA_ARGS__)
+
+#define __LOG_ARG_N( \
+          _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
+         _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
+         _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
+         _31,_32,_33,_34,_35,_36,_37,_38,_39,_40, \
+         _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
+         _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
+         _61,_62,_63,_64,N,...) N
+
+
+#define __LOG_RSEQ_N() \
+         __LOG_N, __LOG_N, __LOG_N, __LOG_N , \
+         __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, \
+         __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, \
+         __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, \
+         __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, \
+         __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, \
+         __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_N, __LOG_0
 
 #ifndef CYNARA_NO_LOGS
 #include <sstream>
@@ -35,13 +61,16 @@ extern int __log_level;
 #ifndef CYNARA_NO_LOGS
     #define __LOG(LEVEL, FORMAT, ...) \
         do { \
-            if(LEVEL <= __log_level) { \
-                std::stringstream __LOG_MACRO_format; \
-                __LOG_MACRO_format << FORMAT; \
-                sd_journal_print(LEVEL, __LOG_MACRO_format.str().c_str(), ##__VA_ARGS__); \
+            if (LEVEL <= __log_level) { \
+                std::stringstream __LOG_FORMAT; \
+                __LOG_FORMAT << FORMAT; \
+                __LOG_NARG(_0, ##__VA_ARGS__) (LEVEL, __LOG_FORMAT.str().c_str(), ##__VA_ARGS__); \
             } \
         } while (0)
-#else
+
+    #define __LOG_N(LEVEL, FORMAT, ...) sd_journal_print(LEVEL, FORMAT, ##__VA_ARGS__)
+    #define __LOG_0(LEVEL, FORMAT, ...) sd_journal_print(LEVEL, "%s", FORMAT)
+#else // CYNARA_NO_LOGS
     #define __LOG(LEVEL, ...)
 #endif
 
