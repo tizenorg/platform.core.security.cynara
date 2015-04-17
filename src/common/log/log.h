92fa4b551695e002199268fc5d2a9c7a94329a11
Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
2015-04-17 21:19:06 +0200

    Make compilation with systemd configurable
    All places where cynara is dependent on systemd are compiled conditionaly
    if BUILD_WITH_SYSTEMD is defined. That value is defined in spec file and
    passed to cmake call during package build, but is not required for cmake
    to work properly. So cmake can be used manually without it.
    
    Change-Id: I0c0a3fb1601a556131b1ae60ef29131fd483e955

--- src/common/log/log.h
+++ src/common/log/log.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ * Copyright (c) 2014-2015 Samsung Electronics Co., Ltd All Rights Reserved
  *
  *  Contact: Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
  *
@@ -18,6 +18,7 @@
 /**
  * @file        src/common/log/log.h
  * @author      Adam Malinowski <a.malinowsk2@partner.samsung.com>
+ * @author      Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
  * @version     1.0
  * @brief       This file defines logging utilities.
  */
@@ -52,8 +53,12 @@
 
 #ifndef CYNARA_NO_LOGS
 #include <sstream>
+#ifdef BUILD_WITH_SYSTEMD
 #include <systemd/sd-journal.h>
-#endif
+#else // BUILD_WITH_SYSTEMD
+#include <syslog.h>
+#endif // BUILD_WITH_SYSTEMD
+#endif // CYNARA_NO_LOGS
 
 extern int __log_level;
 
@@ -67,8 +72,14 @@ extern int __log_level;
             } \
         } while (0)
 
+#ifdef BUILD_WITH_SYSTEMD
     #define __LOG_N(LEVEL, FORMAT, ...) sd_journal_print(LEVEL, FORMAT, ##__VA_ARGS__);
     #define __LOG_0(LEVEL, FORMAT, ...) sd_journal_print(LEVEL, "%s", FORMAT);
+#else // BUILD_WITH_SYSTEMD
+    #define __LOG_N(LEVEL, FORMAT, ...) syslog(LEVEL, FORMAT, ##__VA_ARGS__);
+    #define __LOG_0(LEVEL, FORMAT, ...) syslog(LEVEL, "%s", FORMAT);
+#endif // BUILD_WITH_SYSTEMD
+
 #else // CYNARA_NO_LOGS
     #define __LOG(LEVEL, ...)
 #endif
