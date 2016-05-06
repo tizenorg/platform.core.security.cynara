59d83195813d0bcde4b809bceacf631f88b7be7d
Zofia Abramowska <z.abramowska@samsung.com>
2016-05-06 17:44:01 +0200

    Add flush tests
    Change-Id: I6ab821919a26a11c87252dc5e3e6e58e879da76c

--- test/service/monitor/entriesmanager.cpp
+++ test/service/monitor/entriesmanager.cpp
@@ -284,3 +284,49 @@ TEST(EntriesManager, user8) {
     ASSERT_THAT(entriesManager.fetchEntriesForClient(666), ElementsAre(entry3, entry4));
     ASSERT_THAT(entriesManager.fetchEntriesForClient(777), ElementsAre(entry1, entry2));
 }
+
+TEST(EntriesManager, userFlush) {
+    using ::testing::ElementsAre;
+
+    EntriesManager entriesManager;
+    MonitorEntry entry1{{"c1", "u", "p"}, 0, {0, 0}};
+    MonitorEntry entry2{{"c2", "u", "p"}, 0, {0, 0}};
+    MonitorEntry entry3{{"c3", "u", "p"}, 0, {0, 0}};
+
+    ASSERT_TRUE(entriesManager.addClient(666, 4));
+
+    ASSERT_TRUE(entriesManager.addEntry(entry1));
+    ASSERT_TRUE(entriesManager.addEntry(entry2));
+    ASSERT_TRUE(entriesManager.addEntry(entry3));
+
+    ASSERT_THAT(entriesManager.fetchEntriesForClient(666, true), ElementsAre(entry1, entry2, entry3));
+}
+
+TEST(EntriesManager, userFlushNone) {
+    using ::testing::ElementsAre;
+
+    EntriesManager entriesManager;
+    ASSERT_TRUE(entriesManager.addClient(666, 4));
+    ASSERT_THAT(entriesManager.fetchEntriesForClient(666, true), ElementsAre());
+}
+
+TEST(EntriesManager, userFetchFlush) {
+    using ::testing::ElementsAre;
+
+    EntriesManager entriesManager;
+    MonitorEntry entry1{{"c1", "u", "p"}, 0, {0, 0}};
+    MonitorEntry entry2{{"c2", "u", "p"}, 0, {0, 0}};
+    MonitorEntry entry3{{"c3", "u", "p"}, 0, {0, 0}};
+    MonitorEntry entry4{{"c4", "u", "p"}, 0, {0, 0}};
+
+    ASSERT_TRUE(entriesManager.addClient(666, 3));
+
+    ASSERT_TRUE(entriesManager.addEntry(entry1));
+    ASSERT_TRUE(entriesManager.addEntry(entry2));
+    ASSERT_TRUE(entriesManager.addEntry(entry3));
+    ASSERT_THAT(entriesManager.fetchEntriesForClient(666), ElementsAre(entry1, entry2, entry3));
+
+    ASSERT_TRUE(entriesManager.addEntry(entry4));
+    ASSERT_TRUE(entriesManager.modifyClient(666, 3));
+    ASSERT_THAT(entriesManager.fetchEntriesForClient(666, true), ElementsAre(entry4));
+}
